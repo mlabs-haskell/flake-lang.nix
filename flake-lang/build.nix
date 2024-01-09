fe37ac115998717840ad4041d8f0c64127a6e7f4
jared <>
2024-01-09 03:15:36 -0700

    Added more precise types + documentation improvements

--- flake-lang/build.nix
+++ flake-lang/build.nix
@@ -2,7 +2,7 @@
 
 # Loosely, the key idea is that in this flake we want to have an attribute like
 # ```
-# lbf-nix.<system> = {
+# lib.<system> = {
 #   haskellFlake = ...
 #   rustFlake = ...
 #   typescriptFlake = ...
@@ -14,54 +14,79 @@
 # - [1] https://github.com/hercules-ci/flake-parts/blob/main/lib.nix
 # - [2] https://github.com/hercules-ci/flake-parts/pull/63/files
 # - [3] https://github.com/hercules-ci/flake-parts/blob/main/modules/formatter.nix
-
 { config, inputs, flake-parts-lib, lib, ... }: {
   options = {
+    flake = flake-parts-lib.mkSubmoduleOptions {
+      lib = lib.mkOption {
+        type = lib.types.lazyAttrsOf (lib.types.lazyAttrsOf (lib.types.functionTo lib.types.attrs));
+        default = { };
+        description = ''
+          Nix functions to help create flakes for languages.
+
+          TODO(jaredponn): document this better.
+        '';
+      };
+    };
     perSystem = flake-parts-lib.mkPerSystemOption ({ pkgs, pkgsForCtl, pkgsForHaskellNix, pkgsForRust, ... }: {
       options = {
         lib = {
           purescriptFlake = lib.mkOption {
-            type = lib.types.functionTo lib.types.raw;
+            type = lib.types.functionTo lib.types.attrs;
             default = import ./flake-purescript.nix pkgsForCtl;
             readOnly = true;
             description = ''
               TODO(jaredponn): write down documentation here
             '';
+            example = ''
+              TODO(jaredponn): write down an example here
+            '';
           };
 
           rustFlake = lib.mkOption {
-            type = lib.types.functionTo lib.types.raw;
+            type = lib.types.functionTo lib.types.attrs;
             default = import ./flake-rust.nix pkgsForRust;
             readOnly = true;
             description = ''
               TODO(jaredponn): write down documentation here
             '';
+            example = ''
+              TODO(jaredponn): write down an example here
+            '';
           };
 
           haskellFlake = lib.mkOption {
-            type = lib.types.functionTo lib.types.raw;
+            type = lib.types.functionTo lib.types.attrs;
             default = import ./flake-haskell.nix pkgsForHaskellNix;
             readOnly = true;
             description = ''
               TODO(jaredponn): write down documentation here
             '';
+            example = ''
+              TODO(jaredponn): write down an example here
+            '';
           };
 
           haskellPlutusFlake = lib.mkOption {
-            type = lib.types.functionTo lib.types.raw;
+            type = lib.types.functionTo lib.types.attrs;
             default = import ./flake-haskell-plutus.nix inputs.cardano-haskell-packages pkgsForHaskellNix;
             readOnly = true;
             description = ''
               TODO(jaredponn): write down documentation here
             '';
+            example = ''
+              TODO(jaredponn): write down an example here
+            '';
           };
 
           typescriptFlake = lib.mkOption {
-            type = lib.types.functionTo lib.types.raw;
+            type = lib.types.functionTo lib.types.attrs;
             default = import ./flake-typescript.nix pkgs;
             readOnly = true;
             description = ''
               TODO(jaredponn): write down documentation here
+            '';
+            example = ''
+              TODO(jaredponn): write down an example here
             '';
           };
 
@@ -71,7 +96,16 @@
   };
 
   config = {
+    # TODO(jaredponn): not sure why the new fancy `transposition` submodule
+    # doesn't set the `perInput` correctly? So, when consuming this module, we
+    # can't do something like
+    # ```
+    # inputs'.flake-lang.lib.haskellFlake { ... }
+    # ```
+    # and instead must do
+    # ```
+    # inputs.flake-lang.lib.${system}.haskellFlake { ... }
+    # ```
     transposition.lib = { };
   };
-
 }
