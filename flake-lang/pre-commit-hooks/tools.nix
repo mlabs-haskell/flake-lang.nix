6dd16b6f7291439f8f0e85cfb00da5b8626bd762
jared <>
2024-02-06 04:00:40 -0700

    Moved `_module.args` to a `flake-lang-pkgs` option

--- flake-lang/pre-commit-hooks/tools.nix
+++ flake-lang/pre-commit-hooks/tools.nix
@@ -1,12 +1,12 @@
 { flake-parts-lib, lib, ... }: {
   imports = [ ../../pkgs.nix ];
   options = {
-    perSystem = flake-parts-lib.mkPerSystemOption ({ pkgsForRust, ... }: {
+    perSystem = flake-parts-lib.mkPerSystemOption ({ config, ... }: {
       options = {
         flake-lang.pre-commit-hooks.tools = {
           rustfmt = lib.mkOption {
             type = lib.types.package;
-            default = pkgsForRust.rustfmt;
+            default = config.flake-lang-pkgs.pkgsForRust.rustfmt;
             readOnly = false;
             description = lib.mdDoc ''Rust formatter to use for pre-commit hooks'';
           };
