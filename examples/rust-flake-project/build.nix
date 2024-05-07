278c4467ba888b2fc59c2f11c5b8d8aaaafec16d
Hongrui Fang <chfanghr@gmail.com>
2024-05-07 17:50:32 +0800

    rust: showcase cross compilation

--- examples/rust-flake-project/build.nix
+++ examples/rust-flake-project/build.nix
@@ -1,19 +1,33 @@
-{ ... }: {
+{ lib, ... }: {
   perSystem = { config, ... }:
 
     let
-      rustFlake = config.lib.rustFlake
-        {
-          src = ./.;
-          crateName = "rust-flake-project";
+      inherit (builtins) mapAttrs;
+      inherit (lib) mapAttrs' nameValuePair recursiveUpdate;
 
-          devShellHook = config.settings.shell.hook;
+      commonArgs = {
+        src = ./.;
+        crateName = "rust-flake-project";
 
-        };
+        devShellHook = config.settings.shell.hook;
+      };
+
+      rustFlake = config.lib.rustFlake commonArgs;
+
+      rustFlakeMusl = config.lib.rustFlake (commonArgs // {
+        target = "x86_64-unknown-linux-musl";
+        extraRustcFlags = "-C target-feature=+crt-static";
+      });
+
+      addMuslSuffixToAttrNames = mapAttrs' (name: nameValuePair "${name}-musl");
+
+      rustFlakeMusl' = mapAttrs (_: addMuslSuffixToAttrNames) rustFlakeMusl;
     in
-    {
-
-      inherit (rustFlake) packages checks devShells;
-
-    };
+    recursiveUpdate
+      {
+        inherit (rustFlake) packages checks devShells;
+      }
+      {
+        inherit (rustFlakeMusl') packages checks devShells;
+      };
 }
