5c815abd51e86c3f5e3c040d8b7e35df0114a4b1
Hongrui Fang <chfanghr@gmail.com>
2024-05-07 17:20:54 +0800

    rust: output valid flake

--- flake-lang/flake-rust.nix
+++ flake-lang/flake-rust.nix
@@ -17,171 +17,155 @@ inputCrane: pkgs:
 , testTools ? [ ]
 , cargoNextestExtraArgs ? ""
 , doInstallCargoArtifacts ? false
-, extraTargets ? [ ]
+, target ? null
+, extraRustcFlags ? null
+, extraCargoArgs ? null
+, extraEnvVars ? null
 }:
 
 let
-  mkFlakeForTarget = { target, extraRustcFlags ? null, extraCargoArgs ? null }:
+  inherit (pkgs.lib) optionalAttrs;
+
+  rustWithTools = pkgs.rust-bin.${rustProfile}.${rustVersion}.default.override
+    ({
+      extensions = [ "rustfmt" "rust-analyzer" "clippy" "rust-src" ];
+    } // optionalAttrs (target != null) {
+      targets = [ target ];
+    });
+
+  craneLib =
     let
-      inherit (pkgs.lib) optionalAttrs;
-      rustWithTools = pkgs.rust-bin.${rustProfile}.${rustVersion}.default.override
-        ({
-          extensions = [ "rustfmt" "rust-analyzer" "clippy" "rust-src" ];
-        } // optionalAttrs (target != null) {
-          targets = [ target ];
-        });
+      crane' =
+        if crane == null then
+          inputCrane
+        else
+          pkgs.lib.showWarnings [ ''rustFlake: You're setting the `crane` argument which is deprecated and will be removed in the next major revision'' ] crane;
 
-      craneLib =
-        let
-          crane' =
-            if crane == null then
-              inputCrane
-            else
-              pkgs.lib.showWarnings [ ''rustFlake: You're setting the `crane` argument which is deprecated and will be removed in the next major revision'' ] crane;
-
-        in
-        crane'.lib.${pkgs.system}.overrideToolchain rustWithTools;
-
-      cleanSrc = craneLib.cleanCargoSource (craneLib.path src);
-
-      # Library source code with extra dependencies copied
-      buildEnv =
-        pkgs.stdenv.mkDerivation
-          {
-            src = cleanSrc;
-            name = "${crateName}-build-env";
-            unpackPhase = ''
-              mkdir $out
-              cp -r $src/* $out
-              cd $out
-              ${copyExtraSources}
-              ${copyData}
-            '';
-          };
-
-      # Library source code, intended to be used in extraSources
-      # Dependencies of this crate are not copied, to the extra sources directory
-      # but they are referenced from the parent directory (parent crate's extra sources).
-      vendoredSrc =
-        pkgs.stdenv.mkDerivation
-          {
-            src = cleanSrc;
-            name = "${crateName}-${version}";
-            unpackPhase = ''
-              mkdir $out
-              cp -r $src/* $out
-              cd $out
-              sed -Ei 's/${pkgs.lib.escapeRegex extraSourcesDir}/../g' Cargo.toml
-            '';
-          };
-
-
-      defNativeBuildInputs =
-        (pkgs.lib.optionals pkgs.stdenv.isLinux [
-          pkgs.pkg-config
-        ]) ++
-        (pkgs.lib.optionals pkgs.stdenv.isDarwin
-          [
-            pkgs.gcc
-            pkgs.darwin.apple_sdk.frameworks.Security
-            pkgs.darwin.apple_sdk.frameworks.SystemConfiguration
-          ]);
-      defBuildInputs = [
-        pkgs.openssl.dev
-      ];
-
-      commonArgs = {
-        nativeBuildInputs = defNativeBuildInputs ++ nativeBuildInputs;
-        buildInputs = defBuildInputs ++ buildInputs;
-        src = buildEnv;
-        pname = crateName;
-        strictDeps = true;
-      } // optionalAttrs (target != null) {
-        CARGO_BUILD_TARGET = target;
-      } // optionalAttrs (extraRustcFlags != null) {
-        CARGO_BUILD_RUSTFLAGS = extraRustcFlags;
-      } // optionalAttrs (extraCargoArgs != null) {
-        cargoExtraArgs = extraCargoArgs;
-      };
-
-      cargoArtifacts = craneLib.buildDepsOnly commonArgs;
-
-      # Extra sources
-      extra-sources = pkgs.linkFarm "extra-sources" (builtins.map (drv: { name = drv.name; path = drv; }) extraSources);
-
-      hasExtraSources = builtins.length extraSources > 0;
-      linkExtraSources = pkgs.lib.optionalString hasExtraSources ''
-        echo "Linking extra sources"
-        if [ -e ./${extraSourcesDir} ]; then rm ./${extraSourcesDir}; fi
-        ln -s ${extra-sources} ./${extraSourcesDir}
-      '';
-      copyExtraSources = pkgs.lib.optionalString hasExtraSources ''
-        echo "Copying extra sources"
-        cp -Lr ${extra-sources} ./${extraSourcesDir}
-      '';
-
-      # Data
-      data-drv = pkgs.linkFarm "data" data;
-      hasData = builtins.length data > 0;
-      linkData = pkgs.lib.optionalString hasData ''
-        echo "Linking data"
-        if [ -e ./${dataDir} ]; then rm ./${dataDir}; fi
-        ln -s ${data-drv} ./${dataDir}
-      '';
-      copyData = pkgs.lib.optionalString hasData ''
-        echo "Copying data"
-        cp -Lr ${data-drv} ./${dataDir}
-      '';
     in
-    {
-      devShells."dev-${crateName}-rust" = craneLib.devShell {
-        buildInputs = commonArgs.buildInputs ++ commonArgs.nativeBuildInputs;
-        packages = devShellTools ++ [ pkgs.cargo-nextest ] ++ testTools;
-        shellHook = ''
-          ${linkExtraSources}
-          ${linkData}
-          ${devShellHook}
+    crane'.lib.${pkgs.system}.overrideToolchain rustWithTools;
+
+  cleanSrc = craneLib.cleanCargoSource (craneLib.path src);
+
+  # Library source code with extra dependencies copied
+  buildEnv =
+    pkgs.stdenv.mkDerivation
+      {
+        src = cleanSrc;
+        name = "${crateName}-build-env";
+        unpackPhase = ''
+          mkdir $out
+          cp -r $src/* $out
+          cd $out
+          ${copyExtraSources}
+          ${copyData}
         '';
       };
 
-      packages = {
-        "${crateName}-rust" = craneLib.buildPackage (commonArgs // {
-          inherit cargoArtifacts;
-          doCheck = false;
-          inherit doInstallCargoArtifacts;
-        });
-
-        "${crateName}-rust-src" = vendoredSrc;
-
-        "${crateName}-rust-build-env" = buildEnv;
+  # Library source code, intended to be used in extraSources
+  # Dependencies of this crate are not copied, to the extra sources directory
+  # but they are referenced from the parent directory (parent crate's extra sources).
+  vendoredSrc =
+    pkgs.stdenv.mkDerivation
+      {
+        src = cleanSrc;
+        name = "${crateName}-${version}";
+        unpackPhase = ''
+          mkdir $out
+          cp -r $src/* $out
+          cd $out
+          sed -Ei 's/${pkgs.lib.escapeRegex extraSourcesDir}/../g' Cargo.toml
+        '';
       };
 
-      checks = {
-        "${crateName}-rust-test" = craneLib.cargoNextest (commonArgs // {
-          inherit cargoArtifacts cargoNextestExtraArgs;
-          nativeBuildInputs = commonArgs.nativeBuildInputs ++ testTools;
-        });
 
-        "${crateName}-rust-clippy" = craneLib.cargoClippy (commonArgs // {
-          inherit cargoArtifacts;
-        });
-      };
-    };
+  defNativeBuildInputs =
+    (pkgs.lib.optionals pkgs.stdenv.isLinux [
+      pkgs.pkg-config
+    ]) ++
+    (pkgs.lib.optionals pkgs.stdenv.isDarwin
+      [
+        pkgs.gcc
+        pkgs.darwin.apple_sdk.frameworks.Security
+        pkgs.darwin.apple_sdk.frameworks.SystemConfiguration
+      ]);
+  defBuildInputs = [
+    pkgs.openssl.dev
+  ];
 
-  defaultFlake = mkFlakeForTarget { target = null; };
+  commonArgs = {
+    nativeBuildInputs = defNativeBuildInputs ++ nativeBuildInputs;
+    buildInputs = defBuildInputs ++ buildInputs;
+    src = buildEnv;
+    pname = crateName;
+    strictDeps = true;
+  } // optionalAttrs (target != null) {
+    CARGO_BUILD_TARGET = target;
+  } // optionalAttrs (extraRustcFlags != null) {
+    CARGO_BUILD_RUSTFLAGS = extraRustcFlags;
+  } // optionalAttrs (extraCargoArgs != null) {
+    cargoExtraArgs = extraCargoArgs;
+  } // optionalAttrs (extraEnvVars != null) extraEnvVars;
 
-  extraTargetFlakes =
-    let inherit (builtins) map listToAttrs;
-      inherit (pkgs.lib) nameValuePair;
-    in
-    listToAttrs (map
-      (t:
-        let
-          k = t.target;
-          v = mkFlakeForTarget t;
-        in
-        nameValuePair k v)
-      extraTargets);
+  cargoArtifacts = craneLib.buildDepsOnly commonArgs;
 
+  # Extra sources
+  extra-sources = pkgs.linkFarm "extra-sources" (builtins.map (drv: { name = drv.name; path = drv; }) extraSources);
+
+  hasExtraSources = builtins.length extraSources > 0;
+  linkExtraSources = pkgs.lib.optionalString hasExtraSources ''
+    echo "Linking extra sources"
+    if [ -e ./${extraSourcesDir} ]; then rm ./${extraSourcesDir}; fi
+    ln -s ${extra-sources} ./${extraSourcesDir}
+  '';
+  copyExtraSources = pkgs.lib.optionalString hasExtraSources ''
+    echo "Copying extra sources"
+    cp -Lr ${extra-sources} ./${extraSourcesDir}
+  '';
+
+  # Data
+  data-drv = pkgs.linkFarm "data" data;
+  hasData = builtins.length data > 0;
+  linkData = pkgs.lib.optionalString hasData ''
+    echo "Linking data"
+    if [ -e ./${dataDir} ]; then rm ./${dataDir}; fi
+    ln -s ${data-drv} ./${dataDir}
+  '';
+  copyData = pkgs.lib.optionalString hasData ''
+    echo "Copying data"
+    cp -Lr ${data-drv} ./${dataDir}
+  '';
 in
-defaultFlake // { extraTargets = extraTargetFlakes; }
+{
+  devShells."dev-${crateName}-rust" = craneLib.devShell {
+    buildInputs = commonArgs.buildInputs ++ commonArgs.nativeBuildInputs;
+    packages = devShellTools ++ [ pkgs.cargo-nextest ] ++ testTools;
+    shellHook = ''
+      ${linkExtraSources}
+      ${linkData}
+      ${devShellHook}
+    '';
+  };
+
+  packages = {
+    "${crateName}-rust" = craneLib.buildPackage (commonArgs // {
+      inherit cargoArtifacts;
+      doCheck = false;
+      inherit doInstallCargoArtifacts;
+    });
+
+    "${crateName}-rust-src" = vendoredSrc;
+
+    "${crateName}-rust-build-env" = buildEnv;
+  };
+
+  checks = {
+    "${crateName}-rust-test" = craneLib.cargoNextest (commonArgs // {
+      inherit cargoArtifacts cargoNextestExtraArgs;
+      nativeBuildInputs = commonArgs.nativeBuildInputs ++ testTools;
+    });
+
+    "${crateName}-rust-clippy" = craneLib.cargoClippy (commonArgs // {
+      inherit cargoArtifacts;
+    });
+  };
+}
