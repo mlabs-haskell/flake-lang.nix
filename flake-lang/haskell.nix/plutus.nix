62f431c2f8ec57525201fd74c2a0b9b5c186b87a
Szabo Gergely <gege251@mailbox.org>
2024-11-28 12:50:07 +0000

    Fix plutus flake

--- flake-lang/haskell.nix/plutus.nix
+++ flake-lang/haskell.nix/plutus.nix
@@ -3,46 +3,10 @@ compiler-nix-name: cardano-haskell-packages:
 { lib, config, pkgs, ... }:
 let
   pkgs' = pkgs;
-  brokenLibsModule =
-    let
-      responseFile = builtins.toFile "response-file" ''
-        --optghc=-XFlexibleContexts
-        --optghc=-Wwarn
-        --optghc=-fplugin-opt=PlutusTx.Plugin:defer-errors
-      '';
-      l = [
-        "cardano-binary"
-        "cardano-crypto-class"
-        "cardano-crypto-praos"
-        "cardano-prelude"
-        "heapwords"
-        "measures"
-        "strict-containers"
-        "cardano-ledger-byron"
-        "cardano-slotting"
-      ];
-    in
-    {
-      _file = "flake-lang.nix/flake-lang/haskell.nix/plutus.nix:brokenLibsModule";
-      packages = builtins.listToAttrs (builtins.map
-        (name: {
-          inherit name;
-          value.components.library = {
-            setupHaddockFlags = [ "--haddock-options=@${responseFile}" ];
-            ghcOptions = [ "-XFlexibleContexts" "-Wwarn" "-fplugin-opt=PlutusTx.Plugin:defer-errors" ];
-            extraSrcFiles = [ responseFile ];
-          };
-        })
-        l);
-    };
-  module = { pkgs, ... }: {
+  module = _: {
     _file = "flake-lang.nix/flake-lang/haskell.nix/plutus.nix:module";
     # FIXME: contentAddressed = true;
     reinstallableLibGhc = false; # See https://github.com/input-output-hk/haskell.nix/issues/1939
-    packages = {
-      cardano-crypto-class.components.library.pkgconfig = pkgs.lib.mkForce [ [ pkgs.libsodium-vrf pkgs.secp256k1 pkgs.libblst ] ];
-      cardano-crypto-praos.components.library.pkgconfig = pkgs.lib.mkForce [ [ pkgs.libsodium-vrf ] ];
-    };
   };
 in
 {
@@ -50,7 +14,7 @@ in
   config = {
     cabalProjectLocal = builtins.readFile ./cabal.project.local;
     inherit compiler-nix-name;
-    modules = [ module brokenLibsModule ];
+    modules = [ module ];
     inputMap."https://input-output-hk.github.io/cardano-haskell-packages" = "${cardano-haskell-packages}";
     shell = {
       withHoogle = lib.mkOverride 999 false; # FIXME set to true
