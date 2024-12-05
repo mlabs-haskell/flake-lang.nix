423cfe229e498d0435a95c7733bf56f433ce8aaf
Hongrui Fang <chfanghr@gmail.com>
2024-12-05 12:51:57 +0800

    allow flake-lang to be used on aarch64-darwin and aarch64-linux

--- templates/haskell/flake.nix
+++ templates/haskell/flake.nix
@@ -10,6 +10,8 @@
       systems = [
         "x86_64-linux"
         "x86_64-darwin"
+        "aarch64-linux"
+        "aarch64-darwin"
       ];
 
       imports = [
--- templates/rust-monorepo/flake.nix
+++ templates/rust-monorepo/flake.nix
@@ -11,6 +11,8 @@
       systems = [
         "x86_64-linux"
         "x86_64-darwin"
+        "aarch64-linux"
+        "aarch64-darwin"
       ];
 
       imports = [
