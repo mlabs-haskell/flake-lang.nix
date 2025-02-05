5d6ccdec666f55e38663229fef0e655b37f02875
Hongrui Fang <chfanghr@gmail.com>
2025-02-05 21:26:10 +0800

    fix syntax errors and typos in templates

--- templates/typescript/build.nix
+++ templates/typescript/build.nix
@@ -1,15 +1,16 @@
-{ inputs, ... }{
-perSystem = { system, ... }:
-let
-  typescriptFlake =
-    inputs.flake-lang.lib.${system}.typescriptFlake {
-      name = "example";
-      src = ./.;
-    };
-in
-{
-  inherit (typescriptFlake) checks packages;
+{ inputs, ... }: {
+  perSystem = { system, ... }:
+    let
+      typescriptFlake =
+        inputs.flake-lang.lib.${system}.typescriptFlake {
+          name = "example";
+          src = ./.;
+        };
+    in
+    {
+      inherit (typescriptFlake) checks packages;
 
-  // TODO(chfanghr): Unify the names of dev shells of different lanuages
-  devShells.dev-example-typescript = typescriptFlake.devShells.example-typescript;
+      # # TODO(chfanghr): Unify the names of dev shells of different languages
+      devShells.dev-example-typescript = typescriptFlake.devShells.example-typescript;
+    };
 }
